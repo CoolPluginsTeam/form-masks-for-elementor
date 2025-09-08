c1633e76e5d90383ff96266fbbb090fcf7945ad3
ravi <ravicoolplugins@gmail.com>
2025-09-08 15:00:22 +0530

    improve code

--- admin/marketing/js/fme-form-marketing.js
+++ admin/marketing/js/fme-form-marketing.js
@@ -1,29 +1,22 @@
 jQuery(document).ready(function($) {
 
-
     $(document).on('click', '.fme-dismiss-notice, .fme-dismiss-cross, .fme-tec-notice .notice-dismiss', function(e) {
-
         e.preventDefault();
         var $el = $(this);
         var noticeType = $el.data('notice');
         var nonce = $el.data('nonce');
 
         if (noticeType == undefined) {
-
-            var noticeType = jQuery('.fme-tec-notice').data('notice')
-            var nonce = jQuery('.fme-tec-notice').data('nonce'); 
+            noticeType = jQuery('.fme-tec-notice').data('notice');
+            nonce = jQuery('.fme-tec-notice').data('nonce');
         }
 
         $.post(ajaxurl, {
-
             action: 'fme_mkt_dismiss_notice',
             notice_type: noticeType,
             nonce: nonce
-
         }, function(response) {
-
             if (response.success) {
-
                 if (noticeType === 'cool_form') {
                     $el.closest('.cool-form-wrp').fadeOut();
                 } else if (noticeType === 'tec_notice') {
@@ -31,12 +24,9 @@ jQuery(document).ready(function($) {
                 }
             }
         });
-
     });
 
     $(document).on('click', '.fme-install-plugin', function(e) {
-
-
         e.preventDefault();
 
         var $form = $(this);
@@ -47,124 +37,90 @@ jQuery(document).ready(function($) {
 
         const slug = getPluginSlug(plugin);
         if (!slug) return;
-        // Get the nonce from the button data attribute
+
         let nonce = button.data('nonce');
 
         button.text('Installing...').prop('disabled', true);
         disableAllOtherPluginButtonsTemporarily(slug);
 
         $.post(ajaxurl, {
-
                 action: 'fme_install_plugin',
                 slug: slug,
                 _wpnonce: nonce
             },
-
             function(response) {
-
                 const pluginSlug = slug;
                 const responseString = JSON.stringify(response);
                 const responseContainsPlugin = responseString.includes(pluginSlug);
 
-                
-
-                if(pluginSlug == 'country-code-field-for-elementor-form'){
-
+                if (pluginSlug === 'country-code-field-for-elementor-form') {
                     const $page_html = $(response);
-
                     let $input_country_code = $page_html.find('input[name="country_code"]');
 
-                    if($input_country_code.is(':disabled')){
-                       $wrapper.find('.elementor-control-notice-success').remove();
-                    $wrapper.find('.elementor-control-notice-main-actions').after('<div class="elementor-control-notice elementor-control-notice-success">' + '<div class="elementor-control-notice-content">' +
-                        'The plugin is installed but not yet activated. Please go to the Plugins menu and activate it.' +
-                        '</div></div>');
-                    }else{
-                        button.text('Activated')
-                        .removeClass('e-btn e-info e-btn-1 elementor-button-success')
-                        .addClass('elementor-disabled')
-                        .prop('disabled', true);
-
-                        disableOtherPluginButtons(slug);
-
-                        let successMessage = 'Save & reload the page to start using the feature.';
-
-                        if (slug === 'events-widgets-for-elementor-and-the-events-calendar') {
-
-                            successMessage = 'Events Widget is now active! Design your Events page with Elementor to access powerful new features.';
-                            jQuery('.ect-notice-widget').text(successMessage);
-
-                        } else {
-
-                            $wrapper.find('.elementor-control-notice-success').remove();
-                            $wrapper.find(' .elementor-control-notice-main-actions').after(
-                                '<div class="elementor-control-notice elementor-control-notice-success">' +
-                                '<div class="elementor-control-notice-content">' +
-                                successMessage +
-                                '</div></div>');
-                        }
-                    }
-
-
-
-                }
-
-
-
-
-                else if (responseContainsPlugin) {
-
-                    button.text('Activated')
-                        .removeClass('e-btn e-info e-btn-1 elementor-button-success')
-                        .addClass('elementor-disabled')
-                        .prop('disabled', true);
-
-                    disableOtherPluginButtons(slug);
-
-                    let successMessage = 'Save & reload the page to start using the feature.';
-
-                    if (slug === 'events-widgets-for-elementor-and-the-events-calendar') {
-
-                        successMessage = 'Events Widget is now active! Design your Events page with Elementor to access powerful new features.';
-                        jQuery('.ect-notice-widget').text(successMessage);
-
+                    if ($input_country_code.is(':disabled')) {
+                        showNotActivatedMessage($wrapper);
                     } else {
-
-                        $wrapper.find('.elementor-control-notice-success').remove();
-                        $wrapper.find(' .elementor-control-notice-main-actions').after(
-                            '<div class="elementor-control-notice elementor-control-notice-success">' +
-                            '<div class="elementor-control-notice-content">' +
-                            successMessage +
-                            '</div></div>');
+                        handlePluginActivation(button, slug, $wrapper);
                     }
-
+                } else if (responseContainsPlugin) {
+                    handlePluginActivation(button, slug, $wrapper);
                 } else if (!responseContainsPlugin) {
-
-                    $wrapper.find('.elementor-control-notice-success').remove();
-                    $wrapper.find('.elementor-control-notice-main-actions').after('<div class="elementor-control-notice elementor-control-notice-success">' + '<div class="elementor-control-notice-content">' +
-                        'The plugin is installed but not yet activated. Please go to the Plugins menu and activate it.' +
-                        '</div></div>');
+                    showNotActivatedMessage($wrapper);
                 } else {
-
                     let errorMessage = 'Please try again or download plugin manually from WordPress.org</a>';
                     $wrapper.find('.elementor-button-warning').remove();
                     if (slug === 'events-widget') {
-                        //
                         jQuery('.ect-notice-widget').text(errorMessage)
                     } else {
-
                         $wrapper.find('.elementor-control-notice-main-actions').after(
                             '<div class="elementor-control-notice elementor-button-warning">' +
                             '<div class="elementor-control-notice-content">' +
                             errorMessage +
-                            '</div></div>');
+                            '</div></div>'
+                        );
                     }
                 }
-            });
+            }
+        );
     });
 
-    function getPluginSlug(plugin) {
+    // function for activation success
+    function handlePluginActivation(button, slug, $wrapper) {
+        button.text('Activated')
+            .removeClass('e-btn e-info e-btn-1 elementor-button-success')
+            .addClass('elementor-disabled')
+            .prop('disabled', true);
 
+        disableOtherPluginButtons(slug);
+
+        let successMessage = 'Save & reload the page to start using the feature.';
+
+        if (slug === 'events-widgets-for-elementor-and-the-events-calendar') {
+            successMessage = 'Events Widget is now active! Design your Events page with Elementor to access powerful new features.';
+            jQuery('.ect-notice-widget').text(successMessage);
+        } else {
+            $wrapper.find('.elementor-control-notice-success').remove();
+            $wrapper.find('.elementor-control-notice-main-actions').after(
+                '<div class="elementor-control-notice elementor-control-notice-success">' +
+                '<div class="elementor-control-notice-content">' +
+                successMessage +
+                '</div></div>'
+            );
+        }
+    }
+
+    // function for "not activated" notice
+    function showNotActivatedMessage($wrapper) {
+        $wrapper.find('.elementor-control-notice-success').remove();
+        $wrapper.find('.elementor-control-notice-main-actions').after(
+            '<div class="elementor-control-notice elementor-control-notice-success">' +
+            '<div class="elementor-control-notice-content">' +
+            'The plugin is installed but not yet activated. Please go to the Plugins menu and activate it.' +
+            '</div></div>'
+        );
+    }
+
+    function getPluginSlug(plugin) {
         const slugs = {
             'cool-form-lite': 'extensions-for-elementor-form',
             'conditional': 'conditional-fields-for-elementor-form',
@@ -186,13 +142,11 @@ jQuery(document).ready(function($) {
         jQuery('.fme-install-plugin').each(function() {
             const $btn = jQuery(this);
             const btnSlug = getPluginSlug($btn.data('plugin'));
-
             if (btnSlug !== activeSlug && relatedSlugs.includes(btnSlug)) {
                 $btn.prop('disabled', true);
             }
         });
     }
-
 
     function disableOtherPluginButtons(activatedSlug) {
         const relatedSlugs = [
@@ -215,7 +169,6 @@ jQuery(document).ready(function($) {
 
                 $btn.closest('.cool-form-wrp').hide();
 
-                // Hide associated switcher controls
                 if (btnSlug === 'country-code-field-for-elementor-form') {
                     $('[data-setting="cfme-mkt-country-conditions"]').closest('.elementor-control').hide();
                 }
